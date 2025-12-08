43fb141e4dcb55be292af4cc2877eb6f87f019e1
clearbluejar <3752074+clearbluejar@users.noreply.github.com>
2025-12-08 19:50:47 +0000

    set 11.4.3, update post-install

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -5,12 +5,6 @@ source .venv/bin/activate
 # upgrade pip
 pip install --upgrade pip
 
-# Download latest pyi typings
-pip install ghidra-stubs
-
-# Install pyhdira
-pip install pyghidra
-
 # Install uv
 pip install uv
 
