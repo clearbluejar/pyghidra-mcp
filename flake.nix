4b164898fe31ea854e8083b38f32b07eb384187d
9vvert <mzhm803@gmail.com>
2026-05-17 21:14:38 +0800

    test python package dependency

--- flake.nix
+++ flake.nix
@@ -11,8 +11,31 @@
       pkgs = import nixpkgs {
         inherit system;
       };
+      python = pkgs.python312;
+      py = python.pkgs;
     in
     {
-      packages.${system}.hello = pkgs.hello;
+      packages.${system}.default = py.buildPythonApplication {
+        pname = "pyghidra-mcp";
+        version = "0.2.2";
+
+        src = ./.;
+
+        pyproject = true;
+
+        build-system = with py; [
+          hatchling
+        ];
+
+        # python package dependencies
+        dependencies = with py; [
+          click
+          click-option-group
+          mcp 
+          pyghidra
+          chromadb
+          ghidrecomp
+        ];
+      };
     };
 }
