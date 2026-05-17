c9f52b4bf02537832f362380de39223433cc4309
9vvert <mzhm803@gmail.com>
2026-05-17 23:55:49 +0800

    test flake

--- flake.nix
+++ flake.nix
@@ -11,11 +11,41 @@
       pkgs = import nixpkgs {
         inherit system;
       };
-      python = pkgs.python312;
+      python = pkgs.python313;
       py = python.pkgs;
-    in
-    {
-      packages.${system}.default = py.buildPythonApplication {
+
+      ghidrecomp = py.buildPythonPackage rec {
+        pname = "ghidrecomp";
+        version = "0.5.9";
+        pyproject = true;
+
+        src = pkgs.fetchPypi {
+          inherit pname version;
+          hash = "sha256-ocluLUic2qMREO7kXWum8l3VZ/parj/WtQ9JgOood6I=";
+        };
+
+        build-system = with py; [
+          setuptools
+          wheel
+        ];
+
+        dependencies = with py; [
+          pyghidra
+          jpype1
+          click
+          lxml
+          networkx
+          pydot
+          pyyaml
+          requests
+        ];
+        doCheck = true;
+        pythonImportsCheck = [
+          "ghidrecomp"
+        ];
+      };
+
+      pyghidra-mcp = py.buildPythonApplication {
         pname = "pyghidra-mcp";
         version = "0.2.2";
 
@@ -32,10 +62,43 @@
           click
           click-option-group
           mcp 
-          pyghidra
+          pyghidra    # needs ghidra >= 12.0
           chromadb
           ghidrecomp
+        ]++ py.mcp.optional-dependencies.cli;
+      };
+
+      pyghidra-mcp-cli = py.buildPythonApplication {
+        pname = "pyghidra-mcp-cli";
+        version = "0.2.2";
+
+        src = ./cli;
+        pyproject = true;
+
+        build-system = with py; [
+          hatchling
         ];
+
+        dependencies = with py; [
+          click
+          aiohttp
+          pyghidra-mcp
+        ];
+      };
+    in
+    {
+      # add both binary
+      packages.${system} = {
+        default = pkgs.symlinkJoin {
+          name = "pyghidra-mcp-with-cli";
+
+          paths = [
+              pyghidra-mcp
+              pyghidra-mcp-cli
+            ];
+        };
+        pyghidra-mcp = pyghidra-mcp;
+        pyghidra-mcp-cli = pyghidra-mcp-cli;
       };
     };
 }
---
+++ test.nu
@@ -0,0 +1,4 @@
+nix eval nixpkgs#python312Packages.click-option-group.name
+nix eval nixpkgs#python312Packages.mcp.name
+nix eval nixpkgs#python312Packages.chromadb.name
+nix eval nixpkgs#python312Packages.ghidrecomp.name
